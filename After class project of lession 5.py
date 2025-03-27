d90f3fe5d1dbde82ac8688cbb5a6692d5262637b
spider747344 <9630003948sri@gmail.com>
2025-03-27 19:56:16 +0530

    c:\Users\SRIJAN SARKAR\OneDrive\Desktop\Python classes\lession5\After class project of lession 5.py

---
+++ After class project of lession 5.py
@@ -0,0 +1,8 @@
+i = int(input("enter a tempreature"))
+if(i < 50  ):
+    print("i is Smaller than 50 degree celcius")
+    print("i'm in if Block")
+else:
+    print ("i is greater than 15")
+    print ("i'm in else Block")
+print ("i'm not in if and not in else Block")
